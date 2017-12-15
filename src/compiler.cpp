6fe487411ff1dca932652445f30caad36332ed6f
Kris Jusiak <kris@jusiak.net>
2017-12-15 09:10:43 +0000

    :bug: [osx] clean the fmem buffer each time

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -15,10 +15,10 @@ compiler::compiler(const std::string &uri) : uri_{uri}, compiler_{Compiler_new()
 std::vector<std::string> compiler::compile(const std::unique_ptr<const GherkinDocument, void(*)(const GherkinDocument*)> &document) {
   std::vector<std::string> pickles;
   if (!Compiler_compile(compiler_.get(), document.get())) {
-    fmem fm{};
-    fmem_init(&fm);
-    const auto buffer = fmem_open(&fm, "w");
     while (Compiler_has_more_pickles(compiler_.get())) {
+      fmem fm{};
+      fmem_init(&fm);
+      const auto buffer = fmem_open(&fm, "w");
       fseek(buffer, 0, SEEK_SET);
       const auto event = reinterpret_cast<const Event *>(PickleEvent_new(uri_.c_str(), Compiler_next_pickle(compiler_.get())));
       Event_print(event, buffer);
@@ -29,9 +29,9 @@ std::vector<std::string> compiler::compile(const std::unique_ptr<const GherkinDo
       fmem_mem(&fm, &base, &size);
       std::string pickle{reinterpret_cast<char*>(base), size};
       pickles.emplace_back(pickle);
+      fclose(buffer);
+      fmem_term(&fm);
     }
-    fclose(buffer);
-    fmem_term(&fm);
   }
   return pickles;
 }
